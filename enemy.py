b3357aa6d5b139ebd4a0045e91d22fc17fae4984
Anton-2580 <117103601+Anton-2580@users.noreply.github.com>
2022-12-19 18:12:53 +0300

    Update enemy.py

--- enemy.py
+++ enemy.py
@@ -8,6 +8,17 @@ class Enemy(Wall):
         super().__init__(pos, group, enemy_image, number_pos, transfer, number_room)
         self.image_text = enemy_image
         self.collide = False
+        self.region = pygame.Rect(pos[0] - 160, pos[1] - 160, 320, 320)
+
+    def update_region(self):
+        """
+        функция которая определяет есть ли поблизости объекты с которыми можно взаимодействовать
+
+        :param width: ширена экрана
+        :param height: высота экрана
+        """
+        if self.region.left != self.rect.centerx - 160 or self.region.top != self.rect.centery - 160:
+            self.region = pygame.Rect(self.rect.centerx - 160, self.rect.centery - 160, 320, 320)
 
     def resize(self, width, height, size_block):
         self.image = pygame.image.load(f"textures\\{self.image_text}").convert_alpha()
@@ -18,25 +29,28 @@ class Enemy(Wall):
         self.rect = self.image.get_rect(center=self.rect.center)
 
     def update(self, collider_player: Player, speed: list, key: list, walls):
+        self.update_region()
         self._update(collider_player, speed, key)
         self._go(collider_player)
         self._update_pos(walls)
 
     def _go(self, collider_player):
-        # if collider_player.rect.centerx - 100 > self.rect.centerx:
-        #     self.rect.centerx += 1
-        # elif collider_player.rect.centerx + 100 < self.rect.centerx:
-        #     self.rect.centerx -= 1
-        # if collider_player.rect.centery - 100 > self.rect.centery:
-        #     self.rect.centery += 1
-        # elif collider_player.rect.centery + 100 < self.rect.centery:
-        #     self.rect.centery -= 1
+        # надо проаисать логику подойти ударить отойти
+        if self.region.colliderect(collider_player.rect):
+            if collider_player.rect.centerx - 100 > self.rect.centerx:
+                self.rect.centerx += 1
+            elif collider_player.rect.centerx + 100 < self.rect.centerx:
+                self.rect.centerx -= 1
+            if collider_player.rect.centery - 100 > self.rect.centery:
+                self.rect.centery += 1
+            elif collider_player.rect.centery + 100 < self.rect.centery:
+                self.rect.centery -= 1
         ...
 
     def _update_pos(self, walls):
         # for j in walls:
         #     for i in j:
         #         if self.rect.colliderect(i):
-        #             код выше определяет что враг столкнулся со стеной
+        #             код выше определяет что враг столкнулся со стеной и проходом
         #             объект i это стена или предемет с которым столкнулся враг
         ...
