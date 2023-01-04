6b414502824d8e28c93cef1b7d906e493ea30bb7
Kor1n3110 <96489470+Kor1n3110@users.noreply.github.com>
2023-01-04 11:32:46 +0300

    Update ZLO.py

--- ZLO.py
+++ ZLO.py
@@ -1,48 +1,7 @@
-# import pygame
-#
-#
-# class AnimatedSprite(pygame.sprite.Sprite):
-#     def __init__(self, sheet, columns, rows, x, y):
-#         super().__init__(all_sprites)
-#         self.frames = []
-#         self.cut_sheet(sheet, columns, rows)
-#         self.cur_frame = 0
-#         self.image = self.frames[self.cur_frame]
-#         self.rect = self.rect.move(x, y)
-#
-#     def cut_sheet(self, sheet, columns, rows):
-#         self.rect = pygame.Rect(0, 0, sheet.get_width() // columns,
-#                                 sheet.get_height() // rows)
-#         for j in range(rows):
-#             for i in range(columns):
-#                 frame_location = (self.rect.w * i, self.rect.h * j)
-#                 self.frames.append(sheet.subsurface(pygame.Rect(
-#                     frame_location, self.rect.size)))
-#
-#     def update(self):
-#         self.cur_frame = (self.cur_frame + 1) % len(self.frames)
-#         self.image = self.frames[self.cur_frame]
-#         zlo = AnimatedSprite(load_image("ZLO.png"), 8, 2, 50, 50)
-#
-#
-# size = WIDTH, HEIGHT = 900, 600
-# screen = pygame.display.set_mode(size)
-# clock = pygame.time.Clock()
-# running = True
-# while running:
-#     for event in pygame.event.get():
-#         if event.type == pygame.QUIT:
-#             running = False
-#     screen.fill((0, 0, 0))
-#     pygame.display.update()
-# clock.tick(10)
-# pygame.quit()
-
-
 import pygame
 
 
-class PLAYER(pygame.sprite.Sprite):
+class ZLO(pygame.sprite.Sprite):
     def __init__(self, x, y, file):
         pygame.sprite.Sprite.__init__(self)
         self.image = pygame.image.load(file).convert_alpha()
@@ -67,7 +26,7 @@ pygame.init()
 width, height = 500, 500
 sc = pygame.display.set_mode((width, height))
 clock = pygame.time.Clock()
-player = PLAYER(250, 250, 'image/right/main.png')
+player = ZLO(250, 250, 'image/right/main.png')
 
 while True:
     sc.fill(pygame.Color('white'))
