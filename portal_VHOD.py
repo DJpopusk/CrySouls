71cc3dd4c85a01039a1e1825a43b0fd54f26687b
Anton-2580 <117103601+Anton-2580@users.noreply.github.com>
2023-01-16 00:51:12 +0300

    Update portal_VHOD.py

--- portal_VHOD.py
+++ portal_VHOD.py
@@ -6,38 +6,18 @@ class PORTAL(pygame.sprite.Sprite):
         pygame.sprite.Sprite.__init__(self)
         self.image = pygame.image.load(file).convert_alpha()
         self.rect = self.image.get_rect(center=(x, y))
-        self.dx = 0
-        self.dy = 0
-        self.Go = False
         self.Frame = 0
+        path = ['portal_0.png', 'portal_1.png']
+        self.images = [pygame.image.load(f"image/portalV/{i}").convert_alpha() for i in path]
 
     def update(self, *args):
-        self.rect.x += self.dx
-        self.rect.y += self.dy
-        if self.Go:
-            self.Frame += 0.2
-            if self.Frame > 1:
-                self.Frame -= 1
-            Personnel = ['portal_0.png', 'portal_1.png']
-            self.image = pygame.image.load('image/' + 'portalV' + '/' + Personnel[int(self.Frame)]).convert_alpha()
+        self.animation()
+        self.teleport()
 
+    def teleport(self): ...
 
-pygame.init()
-width, height = 500, 500
-sc = pygame.display.set_mode((width, height))
-clock = pygame.time.Clock()
-player = PORTAL(250, 250, 'image/right/main.png')
-
-while True:
-    sc.fill(pygame.Color('white'))
-    for event in pygame.event.get():
-        if event.type == pygame.QUIT:
-            exit()
-        elif event.type == pygame.KEYUP:
-            player.Go = False
-    key = pygame.key.get_pressed()
-    player.Go = True
-    player.update(height - player.rect.height)
-    sc.blit(player.image, player.rect)
-    pygame.display.update()
-    clock.tick(5)
+    def animation(self):
+        self.Frame += 0.2
+        if self.Frame > 1:
+            self.Frame -= 1
+        self.image = self.images[int(self.Frame)]
