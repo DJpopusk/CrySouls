c2d92a826d09d4db7801b8332fe3975709e60e3d
Anton-2580 <117103601+Anton-2580@users.noreply.github.com>
2023-01-16 20:48:17 +0300

    Update poralExit.py

--- poralExit.py
+++ poralExit.py
@@ -1,17 +1,21 @@
 import pygame
+from wall import Wall
+from player import Player
 
 
-class PortalE(pygame.sprite.Sprite):
-    def __init__(self, x, y, file):
-        pygame.sprite.Sprite.__init__(self)
-        self.image = pygame.image.load(file).convert_alpha()
-        self.rect = self.image.get_rect(center=(x, y))
+class PortalE(Wall):
+    def __init__(self, pos: list, group, portal_image="../image/portalExit/portal_0.PNG", numbers=None, transfer=None,
+                 number_room=0):
+        super().__init__(pos, group, portal_image, numbers, transfer, number_room)
+        self.image_text = portal_image
+        self.collide = False
+
         self.Frame = 0
-
         path = ['portal_0.png', 'portal_1.png']
         self.images = [pygame.image.load(f"image/portalExit/{i}").convert_alpha() for i in path]
 
-    def update(self, *args):
+    def update(self, collider_player: Player, speed: list, key: list):
+        self._update(collider_player, speed, key)
         self.animation()
         self.teleport()
 
@@ -22,4 +26,3 @@ class PortalE(pygame.sprite.Sprite):
         if self.Frame > 1:
             self.Frame -= 1
         self.image = self.images[int(self.Frame)]
-
