198cf492c3b033e5080e6bc4ef161361b711ea5f
Kor1n3110 <96489470+Kor1n3110@users.noreply.github.com>
2023-01-18 23:06:30 +0300

    Add files via upload

---
+++ konec_PROIGRISH.py
@@ -0,0 +1,34 @@
+import pygame
+
+
+def draw(screen):
+    screen.fill((0, 0, 0))
+    font = pygame.font.Font(None, 100)
+    text = font.render("Вы проиграли!", True, (255, 0, 0))
+    text_x = width // 2 - text.get_width() // 2
+    text_y = height // 2 - text.get_height() // 2
+    text_w = text.get_width()
+    text_h = text.get_height()
+    screen.blit(text, (text_x, text_y))
+
+
+
+if __name__ == '__main__':
+    # инициализация Pygame:
+    pygame.init()
+    # размеры окна:
+    size = width, height = 800, 600
+    # screen — холст, на котором нужно рисовать:
+    screen = pygame.display.set_mode(size)
+    # формирование кадра:
+    # команды рисования на холсте
+    # ...
+    # ...
+    # смена (отрисовка) кадра:
+    draw(screen)
+    pygame.display.flip()
+    # ожидание закрытия окна:
+    while pygame.event.wait().type != pygame.QUIT:
+        pass
+    # завершение работы:
+    pygame.quit()
