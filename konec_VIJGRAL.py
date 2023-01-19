1654d746cbbf3061ab36f26accb7ea3d2cdd20a4
Anton-2580 <117103601+Anton-2580@users.noreply.github.com>
2023-01-19 19:18:59 +0300

    Update konec_VIJGRAL.py

--- konec_VIJGRAL.py
+++ konec_VIJGRAL.py
@@ -1,7 +1,7 @@
 import pygame
 
 
-def draw(screen):
+def draw(screen, width, height):
     screen.fill((0, 0, 0))
     font = pygame.font.Font(None, 80)
     text = font.render("Поздравляю, вы выйграли!", True, (0, 255, 0))
@@ -12,23 +12,10 @@ def draw(screen):
     screen.blit(text, (text_x, text_y))
 
 
-
 if __name__ == '__main__':
-    # инициализация Pygame:
     pygame.init()
-    # размеры окна:
     size = width, height = 800, 600
-    # screen — холст, на котором нужно рисовать:
     screen = pygame.display.set_mode(size)
-    # формирование кадра:
-    # команды рисования на холсте
-    # ...
-    # ...
-    # смена (отрисовка) кадра:
-    draw(screen)
+    draw(screen, width, height)
     pygame.display.flip()
-    # ожидание закрытия окна:
-    while pygame.event.wait().type != pygame.QUIT:
-        pass
-    # завершение работы:
     pygame.quit()
